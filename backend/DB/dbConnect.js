c4dafdba04921921e3214ef026ae00a3a4e3d930
Farman <farmanaslam674@gmail.com>
2024-12-29 12:15:24 +0530

    Version 1

--- backend/DB/dbConnect.js
+++ backend/DB/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_CONNECT);
-    console.log("Connected DB");
+    console.log("Connected Database Successfully");
   } catch (error) {
     console.log(error);
   }
